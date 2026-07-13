1c7f408fe63a9946ca2a9c6455266557faf39de7
visakh <visakh>
2026-07-13 22:17:45 +0530

    gol working

--- main.cpp
+++ main.cpp
@@ -5,6 +5,83 @@
 
 #include <chrono>
 #include <thread>
+#include <vector>
+
+#include <cstdlib>
+#include <ctime>
+#include <cstdint>
+#include <X11/keysym.h>
+
+const int CELL_SIZE = 3;
+
+int GRID_W = 0;
+int GRID_H = 0;
+
+std::vector<uint8_t> grid;
+std::vector<uint8_t> nextGrid;
+
+void updateLife();
+void updateLife()
+{
+    for (int y = 0; y < GRID_H; y++)
+    {
+        for (int x = 0; x < GRID_W; x++)
+        {
+            int neighbours = 0;
+
+            for (int yy = -1; yy <= 1; yy++)
+            {
+                for (int xx = -1; xx <= 1; xx++)
+                {
+                    if (xx == 0 && yy == 0)
+                        continue;
+
+                    int nx = (x + xx + GRID_W) % GRID_W;
+                    int ny = (y + yy + GRID_H) % GRID_H;
+
+                    neighbours +=
+                        grid[ny * GRID_W + nx];
+                }
+            }
+
+            bool alive =
+                grid[y * GRID_W + x];
+
+            if (alive)
+            {
+                nextGrid[y * GRID_W + x] =
+                    (neighbours == 2 ||
+                     neighbours == 3);
+            }
+            else
+            {
+                nextGrid[y * GRID_W + x] =
+                    (neighbours == 3);
+            }
+        }
+    }
+
+    grid.swap(nextGrid);
+}
+
+void randomizeGrid()
+{
+    srand(time(nullptr));
+
+    for (auto &c : grid)
+        c = rand() % 5 == 0;
+}
+
+void resizeGrid(int W, int H)
+{
+    GRID_W = (W + CELL_SIZE - 1) / CELL_SIZE;
+    GRID_H = (H + CELL_SIZE - 1) / CELL_SIZE;
+
+    grid.resize(GRID_W * GRID_H);
+    nextGrid.resize(GRID_W * GRID_H);
+
+    randomizeGrid();
+}
 
 static void setAtom(Display *dpy,
                     Window win,
@@ -61,7 +138,10 @@ int main()
     XSetWindowAttributes swa{};
     swa.colormap = cmap;
     swa.override_redirect = True;
-    swa.event_mask = ExposureMask | StructureNotifyMask;
+    swa.event_mask =
+    ExposureMask |
+    StructureNotifyMask |
+    KeyPressMask;
 
     Window win =
         XCreateWindow(
@@ -120,122 +200,146 @@ int main()
 
     glViewport(0, 0, W, H);
 
+    resizeGrid(W,H);
+    // float cell =
+    //     std::min(
+    //         (float)W / GRID_W,
+    //         (float)H / GRID_H);
+
+    float cell = CELL_SIZE;
+
+    float offsetX =
+        (W - GRID_W * cell) * 0.5f;
+
+    float offsetY =
+        (H - GRID_H * cell) * 0.5f;
+
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
 
-    float aspect = (float)W / (float)H;
 
-    glFrustum(
-        -aspect,
-         aspect,
-        -1.0,
-         1.0,
-         1.0,
-         100.0);
+    glOrtho(
+        0,
+        W,
+        H,
+        0,
+        -1,
+        1);
+
+    glMatrixMode(GL_MODELVIEW);
+
+    glLoadIdentity();
+
+    glDisable(GL_DEPTH_TEST);
+
+
+
+    while (true)
+    {
+    while (XPending(dpy))
+    {
+        XEvent e;
+        XNextEvent(dpy, &e);
+
+        if (e.type == KeyPress)
+        {
+            KeySym key =
+                XLookupKeysym(&e.xkey,0);
+
+            if(key == XK_r)
+            {
+                for(auto &c : grid)
+                    c = rand()%5==0;
+            }
+        }
+
+        if (e.type == ConfigureNotify)
+        {
+            W = e.xconfigure.width;
+            H = e.xconfigure.height;
+
+            glViewport(0, 0, W, H);
+
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+
+            glOrtho(
+                0.0,
+                GRID_W,
+                GRID_H,
+                0.0,
+                -1.0,
+                1.0);
+
+            glMatrixMode(GL_MODELVIEW);
+        }
+    }
+
+    //-------------------------------------------------
+    // Update simulation
+    //-------------------------------------------------
+
+    updateLife();
+
+
+    int alive = 0;
+
+    for(auto c : grid)
+        alive += c;
+
+    if(alive < 25)
+    {
+        for(auto &c : grid)
+            c = rand()%5==0;
+    }
+        //-------------------------------------------------
+    // Draw
+    //-------------------------------------------------
+
+    glClearColor(0.05f,0.05f,0.08f,1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    glEnable(GL_DEPTH_TEST);
+    float cell = CELL_SIZE;
 
-    float angle = 0.0f;
+    float offsetX =
+        (W - GRID_W * cell) * 0.5f;
 
-    // ---------------------------
-    // Main Loop
-    // ---------------------------
+    float offsetY =
+        (H - GRID_H * cell) * 0.5f;
 
-    while (true)
+    glBegin(GL_QUADS);
+
+    for(int y=0;y<GRID_H;y++)
     {
-        while (XPending(dpy))
+        for(int x=0;x<GRID_W;x++)
         {
-            XEvent e;
-            XNextEvent(dpy, &e);
+            if(!grid[y*GRID_W+x])
+                continue;
 
-            if (e.type == ConfigureNotify)
-            {
-                W = e.xconfigure.width;
-                H = e.xconfigure.height;
+            glColor3f(
+                0.2f,
+                0.55f,
+                1.0f);
 
-                glViewport(0, 0, W, H);
+            float px = offsetX + x * cell;
+            float py = offsetY + y * cell;
 
-                glMatrixMode(GL_PROJECTION);
-                glLoadIdentity();
-
-                aspect = (float)W / (float)H;
-
-                glFrustum(
-                    -aspect,
-                     aspect,
-                    -1.0,
-                     1.0,
-                     1.0,
-                     100.0);
-
-                glMatrixMode(GL_MODELVIEW);
-            }
+            glVertex2f(px, py);
+            glVertex2f(px + cell, py);
+            glVertex2f(px + cell, py + cell);
+            glVertex2f(px, py + cell);
         }
+    }
 
-        angle += 1.0f;
+    glEnd();
 
-        glClearColor(0.05f, 0.05f, 0.08f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glXSwapBuffers(dpy,win);
 
-        glLoadIdentity();
-
-        glTranslatef(0.0f, 0.0f, -5.0f);
-
-        glRotatef(angle, 1.0f, 1.0f, 0.0f);
-
-        glBegin(GL_QUADS);
-
-        // Front
-        glColor3f(1,0,0);
-        glVertex3f(-1,-1, 1);
-        glVertex3f( 1,-1, 1);
-        glVertex3f( 1, 1, 1);
-        glVertex3f(-1, 1, 1);
-
-        // Back
-        glColor3f(0,1,0);
-        glVertex3f(-1,-1,-1);
-        glVertex3f(-1, 1,-1);
-        glVertex3f( 1, 1,-1);
-        glVertex3f( 1,-1,-1);
-
-        // Left
-        glColor3f(0,0,1);
-        glVertex3f(-1,-1,-1);
-        glVertex3f(-1,-1, 1);
-        glVertex3f(-1, 1, 1);
-        glVertex3f(-1, 1,-1);
-
-        // Right
-        glColor3f(1,1,0);
-        glVertex3f( 1,-1,-1);
-        glVertex3f( 1, 1,-1);
-        glVertex3f( 1, 1, 1);
-        glVertex3f( 1,-1, 1);
-
-        // Top
-        glColor3f(1,0,1);
-        glVertex3f(-1, 1,-1);
-        glVertex3f(-1, 1, 1);
-        glVertex3f( 1, 1, 1);
-        glVertex3f( 1, 1,-1);
-
-        // Bottom
-        glColor3f(0,1,1);
-        glVertex3f(-1,-1,-1);
-        glVertex3f( 1,-1,-1);
-        glVertex3f( 1,-1, 1);
-        glVertex3f(-1,-1, 1);
-
-        glEnd();
-
-        glXSwapBuffers(dpy, win);
-
-        std::this_thread::sleep_for(
-            std::chrono::milliseconds(33));
+    std::this_thread::sleep_for(
+        std::chrono::milliseconds(33));
     }
 
     return 0;
